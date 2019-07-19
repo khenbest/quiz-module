8f5ad358cfcd671a412fc27ca0ae5bb9c29f1ce8
Catherine Buergel <catherinebuergel@gmail.com>
2019-07-19 15:16:20 -0600

    making the forms pretty

--- src/components/CreateQuestionComponent.vue
+++ src/components/CreateQuestionComponent.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="CreateQuestion row">
-    <div class="col-12">
-      <button class="btn btn-info" @click="makeQuestion = !makeQuestion">Create a Question</button>
+    <div class="col-12 d-flex flex-column">
+      <button class="btn btn-info mx-auto" @click="makeQuestion = !makeQuestion">Create a Question</button>
       <form v-if="makeQuestion" @submit.prevent="createQuestion">
-        <div class="form-group">
-          <label for="questionType">Select Type of Question</label>
-          <select class="form-control" id="questionType" v-model="selected">
+        <div class="form-group d-flex justify-content-center flex-column justify-content-center">
+          <strong> <label for="questionType" class="mt-2">Select Type of Question</label></strong>
+          <select class="form-control mx-auto" id="questionType" v-model="selected">
             <option v-for="type in types" :value="type.value">{{type.text}}</option>
           </select>
           <component :is="selected"></component>
@@ -45,6 +45,11 @@
     }
   }
 </script>
+<style>
+  #questionType {
+    width: 25%;
+  }
+</style>
 
 <!-- let schema = new mongoose.Schema({
 prompt: { type: String, required: true },
--- src/components/CreateQuestionTypes/Match.vue
+++ src/components/CreateQuestionTypes/Match.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="Match row">
-    <div class="col-12">
-      <label for="prompt">Question Prompt</label>
-      <input type="text" v-model="newQuestion.prompt" class="form-control" id="prompt">
-      <label for="howManyMatching">How many matching pairs would you like?</label>
-      <input type="number" id="howManyMatching" v-model="numberOfMatches">
+    <div class="col-10 offset-lg-1 d-flex flex-column">
+      <label for="prompt" class="my-2">Question Prompt</label>
+      <!-- <input type="text" v-model="newQuestion.prompt" class="form-control" id="prompt"> -->
+      <textarea placeholder="Question prompt..." v-model="newQuestion.prompt" id="prompt" class-="form-control mx-auto"
+        spellcheck="true"></textarea>
+      <label for="howManyMatching" class="my-2">How many matching pairs would you like?</label>
+      <input class="form-control col-1 mx-auto" type="number" id="howManyMatching" v-model="numberOfMatches">
     </div>
     <div class="col">
-      <div class="row">
-        <div class="col-5 d-flex flex-column">
+      <div v-if="newQuestion.options.length > 0" class="row d-flex justify-content-center">
+        <div class="col-5 d-flex flex-column justify-content-center">
           <h3>Terms</h3>
           <span v-for="option in terms">
-            <input class="form-control" v-model="option.term" type="text">
+            <input class="form-control my-1 col-6 mx-auto" v-model="option.term" type="text">
           </span>
         </div>
-        <div class="col-5 d-flex flex-column">
+        <div class="col-5 d-flex flex-column justify-content-center">
           <h3>definitions</h3>
           <span v-for="option in definitions">
-            <input class="form-control" v-model="option.term" type="text">
+            <input class="form-control my-1" v-model="option.term" type="text">
           </span>
         </div>
       </div>
@@ -70,3 +72,9 @@
     components: {}
   }
 </script>
+<style>
+  textarea {
+    border: 1px solid black;
+    border-radius: .25rem;
+  }
+</style>
