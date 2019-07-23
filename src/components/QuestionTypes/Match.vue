e0a3fe3681ea12af8d6f9b136021c6897902f449
Kenny Henbest <lekenbest@gmail.com>
2019-07-23 16:12:59 -0600

    dragdrop

--- src/components/QuestionTypes/Match.vue
+++ src/components/QuestionTypes/Match.vue
@@ -3,8 +3,10 @@
         {{question.prompt}}
         <div class="d-flex row justify-content-center">
             <div class="col-5">
-                <textarea draggable="true" v-for="prop in question.options" class="form-control mb-1"
-                    :placeholder="displayQuestion(prop)" readonly></textarea>
+                <drag :transfer-data="prop" v-for="prop in question.options">drag</drag>
+                <drop @drop="handleDrop">drop</drop>
+                <textarea draggable="true" class="form-control mb-1" :placeholder="displayQuestion(prop)"
+                    readonly></textarea>
             </div>
             <!-- <div class="col-5">
                 <input v-for="prop in question" class="form-control mb-2" type="text"
@@ -15,6 +17,7 @@
 </template>
 
 <script>
+    import { Drag, Drop } from 'vue-drag-drop'
     export default {
         name: "Match",
         props: {
@@ -31,7 +34,11 @@
             },
             displayQuestion(q) {
                 return `${Object.values(q)[0]}`
-            }
-        }
+            },
+            handleDrop(data) {
+                alert(`You dropped with data: ${JSON.stringify(data)}`);
+            },
+        },
+        components: { Drag, Drop },
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueDragDrop from 'vue-drag-drop';
+
+Vue.use(VueDragDrop);
 
 Vue.config.productionTip = false
 
