d9023ea2f0e48ae54e45bfa3344bade64fc5c1d0
Catherine Buergel <catherinebuergel@gmail.com>
2019-08-01 10:44:42 -0600

    deleted quiz view :womanshrugging:

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,13 +17,8 @@
     </button>
     <div class="row justify-content-center mt-3">
       <div class="col-8">
-        <md-toolbar class="md-warning" md-elevation="1">
-          <h3 class="md-title" style="flex: 1">SearchBar</h3>
-          <md-field md-clearable class="md-toolbar-section-end">
-            <md-input placeholder="Search by name..." v-model="search" @input="searchOnTable" />
-          </md-field>
-        </md-toolbar>
 
+        <searchQuestion></searchQuestion>
       </div>
     </div>
     <div class="row justify-content-center mb-0">
