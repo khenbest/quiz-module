dccca4b5c15f2a91b274009c74a2034d28edf645
Kenny Henbest <lekenbest@gmail.com>
2019-08-27 11:19:52 -0600

    Table Refactor

--- src/components/SearchQuestions.vue
+++ src/components/SearchQuestions.vue
@@ -1,75 +1,31 @@
 <template>
-  <div class="searchQuestions md-theme-default">
-    <md-table-cell>
+  <div class="searchQuestions md-theme-default md-layout">
+    <div class="md-layout-item md-alignment-center-center md-size-33">
       <CategoriesComponent></CategoriesComponent>
-    </md-table-cell>
-    <md-table-cell>
-      <md-field class="has-danger md-theme-default">
-        <label>Search By Prompt</label>
-        <md-input></md-input>
-      </md-field>
-    </md-table-cell>
-    <md-table-cell>
-      <div class="md-layout-item md-size-100 d-flex justify-content-row"><button
-          class="md-button md-raised md-info md-theme-default">
+    </div>
+    <div class="md-layout-item md-size-66 d-flex justify-content-center row">
+      <div class="md-layout-item md-size-66 d-flex justify-content-center row">
+        <md-field class="has-danger md-theme-default">
+          <label>Search By Prompt</label>
+          <md-input></md-input>
+        </md-field>
+      </div>
+      <div class="md-layout-item d-flex align-content-center justify-content-flex-end row">
+        <button class="md-button md-raised md-info md-theme-default">
           <div class="md-ripple">
             <div class="md-button-content">Search</div>
           </div>
         </button>
         <button class="md-button md-raised md-info md-theme-default">
           <div class="md-ripple">
-            <div class="md-button-content">Clear Search</div>
+            <div class="md-button-content">Clear</div>
           </div>
         </button>
       </div>
-    </md-table-cell>
-    <!-- <md-table-cell>
-      <div class="md-layout-item md-size-100"><button class="md-button md-raised md-info md-theme-default">
-          <div class="md-ripple">
-            <div class="md-button-content">Clear Search</div>
-          </div>
-        </button></div>
-    </md-table-cell> -->
-    <!-- <CategoriesComponent></CategoriesComponent>
-    <div class="md-layout-item md-size-33">
-      <md-field>
-        <label>Search By Prompt</label>
-        <md-input v-model="prompt"></md-input>
-      </md-field>
     </div>
-    <div class="md-layout-item md-size-33 text-center d-flex justify-content-row"><button
-        class="md-button md-raised md-info md-theme-default">
-        <div class="md-ripple">
-          <div class="md-button-content">Search</div>
-        </div>
-      </button>
-      <button type="button" class="md-button md-raised md-info md-theme-default">
-        <div class="md-ripple">
-          <div class="md-button-content">Cancel</div>
-        </div>
-      </button></div> -->
-  </div>
-  <!-- <div class="md-layout-item md-medium-size-100 md-size-66">
-      <div class="md-card md-theme-default">
-        <div class="md-card-header" data-background-color="orange">
-          <h4 class="md-title" style="text-shadow: darkgray -1px -1px;">Search By Category</h4>
-        </div>
-        <div>
-          <div class="md-content md-theme-default" table-header-color="orange">
-            <div class="md-content md-theme-default">
-              <CategoriesComponent v-on:change-categories='updateCategories($event)'></CategoriesComponent>
-            </div>
-            <div class="md-layout-item md-size-100 justify-content-center my-2"><button type="button" @click="search"
-                class="md-button md-raised md-warning md-theme-default">
-                <div class="md-ripple">
-                  <div class="md-button-content ">Search</div>
-                </div>
-              </button></div>
-          </div>
-        </div>
-      </div>
-    </div> -->
 
+  </div>
+  </div>
 </template>
 
 <script>
